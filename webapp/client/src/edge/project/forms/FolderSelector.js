a74d93d0917d6845a4af3c30399f924d2247875d
Yan Xu <yxu@pn1704854.lanl.gov>
2025-09-19 16:12:00 -0600

    Use publicFiles api in FolderSelector

--- webapp/client/src/edge/project/forms/FolderSelector.js
+++ webapp/client/src/edge/project/forms/FolderSelector.js
@@ -68,7 +68,7 @@ export const FolderSelector = (props) => {
     //public files
     var promise3 = new Promise((resolve, reject) => {
       if (props.dataSources.includes('public')) {
-        let serverFiles = postData(apis.userPublicFiles, userData)
+        let serverFiles = postData(apis.publicFiles, userData)
           .then((data) => {
             return data.fileData
           })
