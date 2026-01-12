9846e34a5d761de4f31a6cd201f0c6d5c453bcb0
Yan Xu <yxu@pn2405079.lanl.gov>
2026-01-12 14:17:10 -0700

    Update trame loading duration

--- webapp/server/epic-api/utils/trame.js
+++ webapp/server/epic-api/utils/trame.js
@@ -283,7 +283,7 @@ const startTrame = async (req, res, type) => {
       // wait for the trame server to be up running
       let duration = 3000
       if (trameApp === 'episcope') {
-        duration = 10000
+        duration = 15000
       }
       setTimeout(
         () =>
